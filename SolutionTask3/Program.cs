29d7f9fc5f4da2302758581eb47aa044c62a8340
Golikov-Andrey <newsc2@yandex.ru>
2022-07-08 22:38:21 +0300

    ferst commit

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,21 @@
+﻿string? inputLkneOne = Console.ReadLine();
+
+if(inputLkneOne != null)
+{
+
+    int inputNumber = int.Parse(inputLkneOne);
+
+    int startNumber = 1;
+
+    string lineOutput = "0";
+
+    while (startNumber <= inputNumber)
+    {
+        lineOutput = startNumber*(-1)+", "+lineOutput+", "+startNumber;
+        startNumber++;
+    }
+
+    //lineOutput=lineOutput+inputNumber;
+
+   Console.WriteLine(lineOutput);
+}
---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,21 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+  char[] M = inputLine.ToCharArray();
+  Console.WriteLine(M[2]);
+//    int inputNumber = int.Parse(inputLine);
+
+//    int outputNumber = inputNumber%10;
+
+   //Console.WriteLine(outputNumber);
+}
+
+// string? inputLkneOne = Console.ReadLine();
+
+// if(inputLkneOne != null)
+// {
+//     string lastNum = inputLkneOne.Substring(2,1);
+//     Console.WriteLine(lastNum);
+// }
+
