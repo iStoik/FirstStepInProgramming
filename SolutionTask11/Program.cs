e0d6776d6c5a35a997b05bf9a320b61077cad3f7
Golikov-Andrey <newsc2@yandex.ru>
2022-07-12 21:16:05 +0300

    add solutin for task 11

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,35 @@
+﻿// //Решение Артема
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine("Число:" + number);
+// int numberSecondDigit = number/100;
+// int numberThirdDigit = number%10;
+// Console.WriteLine("Число после удаления второй цифры: " + numberSecondDigit + numberThirdDigit);
+
+//Решение Кирилла
+void middleDigitCutter()
+{
+    int number = new Random().Next(100, 1000);
+    Console.Write("The random number is : ");
+    Console.WriteLine(number);
+    string stringNum = number.ToString();
+    Console.Write("The modified num is : ");
+    Console.Write(stringNum[0]);
+    Console.WriteLine(stringNum[2]);
+}
+
+void middleDigitCutter2() 
+{
+    int number = new Random().Next(100, 1000);
+    Console.Write("The random number is : ");
+    Console.WriteLine(number);
+
+    int num1 = number / 100;
+    int num2 = number % 10;
+
+    Console.Write("The modified num is : ");
+    Console.Write(num1*10 + num2);        
+}
+
+middleDigitCutter();
+middleDigitCutter2();
