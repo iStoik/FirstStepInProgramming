1aded48e1fe511b1957309411eb50023e7ead51c
Golikov-Andrey <newsc2@yandex.ru>
2022-07-12 21:00:41 +0300

    add solution for task 9

--- SolutionTask0/Program.cs
+++ SolutionTask0/Program.cs
@@ -1,11 +1,26 @@
-﻿string? inputLine = Console.ReadLine();
+﻿// string? inputLine = Console.ReadLine();
 
-if(inputLine != null)
+// if(inputLine != null)
+// {
+//    int inputNumber = int.Parse(inputLine);
+
+//    //int outNumber = inputNumber*inputNumber;
+//    int outNumber = (int)Math.Pow(inputNumber,2);
+
+//    Console.WriteLine(outNumber);
+// }
+
+String? InputNum = Console.ReadLine();
+
+if(InputNum != null)
 {
-   int inputNumber = int.Parse(inputLine);
+    int StartNum = 2;
+    string LineOutput = "";
 
-   //int outNumber = inputNumber*inputNumber;
-   int outNumber = (int)Math.Pow(inputNumber,2);
-
-   Console.WriteLine(outNumber);
+    while(StartNum <= (int.Parse(InputNum)))
+    {       
+        LineOutput = LineOutput + StartNum + ", ";        
+        StartNum+=2;
+    }
+    Console.WriteLine(LineOutput.Remove(LineOutput.Length - 2));
 }
---
+++ SolutionTask9/Program.cs
@@ -0,0 +1,68 @@
+﻿
+//Метод решения задачи вариант 1
+void variant1(System.Random numberSintezator)
+{
+    int number = numberSintezator.Next(10, 100);
+
+    Console.WriteLine(number);
+    //Вариант 1
+    int firstNumber = number / 10;
+    int secondNumber = number % 10;
+
+    if (firstNumber > secondNumber)
+    {
+        Console.WriteLine(firstNumber);
+    }
+    else
+    {
+        Console.WriteLine(secondNumber);
+    }
+}
+
+//Метод решения задачи вариант 2
+void variant2()
+{
+    //Вариант 2
+    int numberKirilla = new Random().Next(10, 100);
+
+    Console.Write("The random number is: ");
+    Console.WriteLine(numberKirilla);
+
+    string stringNum = numberKirilla.ToString();
+
+    Console.Write("The biggest digit is : ");
+    if (stringNum[0] > stringNum[1])
+    {
+        Console.WriteLine(stringNum[0]);
+    }
+    else
+    {
+        Console.WriteLine(stringNum[1]);
+    }
+}
+
+//Метод решения задачи вариант 3
+void variant3(System.Random numberSintezator)
+{
+    //Вариант 3 
+
+    char[] digits = numberSintezator.Next(10, 100).ToString().ToCharArray();
+
+    Console.WriteLine(digits);
+
+    int firstNumber = ((int)digits[0]) - 48;
+    int secondNumber = ((int)digits[1]) - 48;
+
+    int resultNumber = firstNumber > secondNumber ? resultNumber = firstNumber : resultNumber = secondNumber;
+
+    Console.WriteLine(resultNumber);
+}
+
+
+System.Random numberSintezator = new Random();
+
+variant1(numberSintezator);
+
+variant2();
+
+variant3(numberSintezator);
